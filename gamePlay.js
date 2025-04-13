c312dc1a87718bbab14ba14e59902fe820188d3b
vishnunath c v <vishnunathcv004@gmail.com>
2025-04-13 14:08:53 +0530

    add option selection function A function wich trigger with when an option is select

--- gamePlay.js
+++ gamePlay.js
@@ -1,6 +1,8 @@
 
   //Main Script
 
+  //Global variables
+
   let prevIndex, nextIndex = -1;
   let totalQuestions;
   let qzid;
@@ -9,6 +11,82 @@
   const prevbtn=document.querySelector(".prev-btn");
   const nextbtn=document.querySelector(".next-btn");
   const submitbtn=document.querySelector(".sub-btn");
+
+  //page functions
+
+  //function to show previous card
+  function prev() {
+    cardStack.swipePrev();
+  }
+  
+  function next() {
+    cardStack.swipeNext();
+  }
+
+  function submit(){
+    console.log("answer submitted");
+  }
+
+  const optionEvent = ()=>{
+    const selectedOption = document.querySelector(`input[name = "option"]:checked`);
+    console.log(selectedOption.value);
+  }
+
+   function eventListnerforOptitons(parentCard){
+      let Options = parentCard.querySelector(".options");
+      Options.addEventListener("change", optionEvent);
+  
+  }
+
+  //Index updating functions.
+  function nextIndexchange(){
+    prevIndex = nextIndex;
+    nextIndex = (nextIndex + 1) % totalQuestions;
+    return nextIndex;
+}
+
+function prevIndexchange(){
+   nextIndex = prevIndex;
+   prevIndex = (prevIndex - 1 + totalQuestions) % totalQuestions;
+   return prevIndex;
+}
+
+
+
+  //quiz ui creating function
+  let createComponent = async (qid = 0) => {
+    const currentQzid = qzid + qid;
+    const currentQuestion = await getQuestion(currentQzid);
+    let component =  `
+          <div class="qcard">
+          <div class="question">
+              ${currentQuestion.question}
+          </div>
+          <div class="options">`;
+
+    currentQuestion.options.forEach(x => {
+      component += `<label class="option ${(x.length > 30) ? "large" : ""}">${x}
+                  <input type="radio" name="option" value="${x}">
+                  <span class="radio"></span>
+              </label>`
+    })
+
+    component += `
+          </div>
+      </div>`;
+    
+    return component;
+  }
+
+  // submit enabling function
+
+  function enableSubmit(index){
+    console.log(index, totalQuestions)
+    if(index === (0))
+      submitbtn.disabled = false;
+
+  }
+
 
   // set event listners to elements
 
@@ -48,63 +126,18 @@
 
   }
 
-  //quiz ui creating function
-  let createComponent = async (qid = 0) => {
-    const currentQzid = qzid + qid;
-    const currentQuestion = await getQuestion(currentQzid);
-    let component = `
-          <div class="qcard">
-          <div class="question">
-              ${currentQuestion.question}
-          </div>
-          <div class="options">`;
 
-    currentQuestion.options.forEach(x => {
-      component += `<label class="option ${(x.length > 30) ? "large" : ""}">${x}
-                  <input type="radio" name="option">
-                  <span class="radio"></span>
-              </label>`
-    })
-
-    component += `
-          </div>
-      </div>`;
-
-    return component;
-  }
-
-  
-  //function to show previous card
-  function prev() {
-    cardStack.swipePrev();
-  }
-  
-  function next() {
-    cardStack.swipeNext();
-  }
-
-  function submit(){
-    console.log("answer submitted");
-  }
-
-  
   //call when next card shows
   cardStack.funcWithNxt = async () => {
-    cardStack.cardsOndesk[1].innerHTML = await createComponent(nextIndexchange());
+    const index = nextIndexchange();
+    cardStack.cardsOndesk[1].innerHTML = await createComponent(index);
+    eventListnerforOptitons(cardStack.cardsOndesk[0]);
+    enableSubmit(index);
   }
   //call with when previous shows 
   cardStack.funcWithPrev =async () => {
     cardStack.cardsOndesk[0].innerHTML = await createComponent(prevIndexchange());
-  }
-  //Index updating functions.
- function nextIndexchange(){
-     prevIndex = nextIndex;
-     nextIndex = (nextIndex + 1) % totalQuestions;
-     return nextIndex;
- }
+    eventListnerforOptitons(cardStack.cardsOndesk[0]);
 
- function prevIndexchange(){
-    nextIndex = prevIndex;
-    prevIndex = (prevIndex - 1 + totalQuestions) % totalQuestions;
-    return prevIndex;
- }
+  }
+
